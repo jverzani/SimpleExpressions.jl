67fcdf0d079545cf3e5f7ac35a852db82e6070b8
jverzani <jverzani@gmail.com>
2024-05-24 15:12:47 -0400

    add extensions for AbstractTrees

---
+++ ext/SimpleExpressionsAbstractTreesExt.jl
@@ -0,0 +1,22 @@
+module SimpleExpressionsAbstractTreesExt
+
+using SimpleExpressions
+import SimpleExpressions: AbstractSymbolic,
+    Symbolic, SymbolicParameter, SymbolicNumber,
+    SymbolicExpression, SymbolicEquation
+
+import AbstractTrees
+
+AbstractTrees.children(::AbstractSymbolic) = ()
+AbstractTrees.children(x::SymbolicExpression) = x.arguments
+AbstractTrees.children(x::SymbolicEquation) = MethodError(AbstractTrees.children, SymbolicExpression)
+
+AbstractTrees.nodevalue(n::Symbolic) = n.x
+AbstractTrees.nodevalue(n::SymbolicParameter) = n.p
+AbstractTrees.nodevalue(n::SymbolicNumber) = n.x
+AbstractTrees.nodevalue(x::SymbolicEquation) = MethodError(AbstractTrees.nodevalue, SymbolicExpression)
+
+
+
+
+end
