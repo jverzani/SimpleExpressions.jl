d1b7cc9d0632d1e69c06c66d68177c0b8f7332f2
jverzani <jverzani@gmail.com>
2024-08-02 20:41:43 -0400

    update for terminterface

---
+++ test/extension_tests.jl
@@ -0,0 +1,17 @@
+# test extensions
+using SimpleExpressions
+@symbolic x p
+
+using Metatheory
+
+r = @rule sin(2(~x)) --> 2sin(~x)*cos(~x)
+@test r(sin(2x)) === 2*sin(x) * cos(x)
+
+t = @theory begin
+    ~a + ~a --> 2 * (~a)
+    ~x / ~x --> 1
+    ~x * 1 --> ~x
+end
+@test isequal(rewrite(x + x, t), 2x)
+@test isequal(rewrite(x/x, t), 1)
+@test isequal(rewrite(1*x, t), x)
