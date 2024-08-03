995490fbef48de533e37b2c94964323b6083c256
jverzani <jverzani@gmail.com>
2024-08-02 21:06:43 -0400

    hold on metatheory

--- test/extension_tests.jl
+++ test/extension_tests.jl
@@ -3,6 +3,18 @@ using SimpleExpressions
 @symbolic x p
 
 using Metatheory
+#=
+need the following
+[compat]
+Metatheory = "3"
+
+[extras]
+Metatheory = "e9d8d322-4543-424a-9be4-0cc815abe26c"
+
+[targets]
+test = ["Test","Metatheory"]
+=#
+
 
 r = @rule sin(2(~x)) --> 2sin(~x)*cos(~x)
 @test r(sin(2x)) === 2*sin(x) * cos(x)
