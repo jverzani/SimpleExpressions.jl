4dc79320818dc700cf3463720b0dd81ed7b892d0
jverzani <jverzani@gmail.com>
2024-12-19 21:28:33 -0500

    additional replace support

--- ext/SimpleExpressionsAbstractTreesExt.jl
+++ ext/SimpleExpressionsAbstractTreesExt.jl
@@ -4,15 +4,15 @@ using SimpleExpressions
 import SimpleExpressions: AbstractSymbolic,
     SymbolicNumber,
     SymbolicExpression, SymbolicEquation
-
+import TermInterface
 import AbstractTrees
 
 # use fallback of () for others
-AbstractTrees.children(x::SymbolicExpression) = x.arguments
+AbstractTrees.children(x::SymbolicExpression) = TermInterface.children(x)
 AbstractTrees.children(x::SymbolicEquation) = MethodError(AbstractTrees.children, SymbolicExpression)
 
-AbstractTrees.nodevalue(n::SymbolicNumber) = n.x
-AbstractTrees.nodevalue(n::SymbolicExpression) = n.op
+AbstractTrees.nodevalue(n::SymbolicNumber) = n()
+AbstractTrees.nodevalue(n::SymbolicExpression) = TermInterface.operation(n)
 AbstractTrees.nodevalue(::SymbolicEquation) = MethodError(AbstractTrees.nodevalue, SymbolicExpression)
 
 
